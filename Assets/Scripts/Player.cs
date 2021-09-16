999f817be088985a0f28cdb54794960fe4048eb5
Alam Rodriguez <alamgrodriguez@gmail.com>
2021-09-15 21:15:56 -0500

    Player movement and Camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,28 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     [SerializeField] private float speed; //Player speed
-    private Vector3 movementDir; //Movement direction of the player
+    private int health; //Player HP
+    [SerializeField] private int maxHealth; //Player Max HP
+    private Vector3 dirMovement; //Movement direction of the player
     private Rigidbody rb;
-
-    public float Speed 
-    {
-        get; set;
-    }
-
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        health = maxHealth;
     }
 
     void Update()
     {
-        movementDir = rb.velocity;
-        movementDir = new Vector3(Input.GetAxisRaw("Horizontal"), movementDir.y, Input.GetAxisRaw("Vertical")).normalized;
+        dirMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized * speed;
 
-        rb.velocity = movementDir * speed;
+        dirMovement.y = rb.velocity.y;
+        rb.velocity = dirMovement;
+
     }
 
+    public float Speed
+    {
+        get; set;
+    }
+
+    public int Health
+    {
+        get { return health; }
+        set
+        {
+            health -= value;
+
+            if (health < 0)
+                health = 0;
+            else if (health > maxHealth)
+                health = maxHealth;
+        }
+    }
 
 }
