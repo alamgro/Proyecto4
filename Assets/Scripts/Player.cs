6e3c0844e908a78b4401d75415f81ecc1e469503
Alam Rodriguez <alamgrodriguez@gmail.com>
2021-09-22 19:48:49 -0500

    Player health (add and substract) also UI
    :rocket: player HP variables, getter and setter.  Health bar in UI update with player's healh.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed; //Player speed
-    private int health; //Player HP
     [SerializeField] private int maxHealth; //Player Max HP
+    [SerializeField] private int health; //Player HP
+    [SerializeField] private Image healthBarUI;
     private Vector3 dirMovement; //Movement direction of the player
     private Rigidbody rb;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        health = maxHealth;
+        health = maxHealth; //Set initial health
+        healthBarUI.fillAmount = (float) health / maxHealth; //Set initial value of HealtBar in UI
     }
 
     void Update()
     {
+        //Get direction vector of movement
         dirMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized * speed;
 
-        dirMovement.y = rb.velocity.y;
+        //Assign movement direction to the rigidbody velocity
+        dirMovement.y = rb.velocity.y; 
         rb.velocity = dirMovement;
 
+        #region DEBUG INPUTS
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Health++;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            Health--;
+        #endregion
     }
 
     public float Speed
@@ -35,12 +46,16 @@ public class Player : MonoBehaviour
         get { return health; }
         set
         {
-            health -= value;
+            health = value;
 
+            //Values verification
             if (health < 0)
                 health = 0;
             else if (health > maxHealth)
                 health = maxHealth;
+
+            //Update health bar
+            healthBarUI.fillAmount = (float)health / maxHealth;
         }
     }
 
