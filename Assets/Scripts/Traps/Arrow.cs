a288d9570fdfaca046a2149d0c44b88b7e21ed92
Alam Rodriguez <alamgrodriguez@gmail.com>
2021-10-01 02:08:21 +0200

    Characters interface
    :rocket: Characters interface to add functions to character when necessary

--- Assets/Scripts/Traps/Arrow.cs
+++ Assets/Scripts/Traps/Arrow.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     [SerializeField] private float initialSpeed;
+    [SerializeField] private int initialDamage; //Damage of the arrow
     [SerializeField] private float despawnTime;
+    private int currentDamage;
     private Rigidbody rb;
-    private Vector3 direction;
 
-    IEnumerator Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        print(direction);
-        rb.AddForce(direction * initialSpeed);
 
-        yield return new WaitForSecondsRealtime(despawnTime);
-        Destroy(gameObject);
+        currentDamage = initialDamage;
     }
 
-    public Vector3 Direction {
-        get { return direction; } 
-        set { direction = value; }
+    void OnEnable()
+    {
+        Invoke(nameof(TurnOffArrow), despawnTime); //Turn off arrow after certain time
+    }
+
+    public void Throw(Vector3 _direction)
+    {
+        //Stop arrow
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        //Throw arrow
+        rb.AddForce(_direction * initialSpeed);
+    }
+
+    private void TurnOffArrow()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Check if the collisioned object has the ICharacter component, meaning that it can take damage
+        ICharacters characters = collision.gameObject.GetComponent<ICharacters>() ;
+        Debug.Log(collision.gameObject, collision.gameObject);
+        if (characters != null)
+        {
+            characters.TakeDamage(currentDamage);
+        }
     }
 
 }
--- Assets/Scripts/Traps/ArrowShooter.cs
+++ Assets/Scripts/Traps/ArrowShooter.cs
@@ -7,10 +7,13 @@ public class ArrowShooter : MonoBehaviour
 {
     [SerializeField] private GameObject pfbArrow; //Arrow prefab
     [SerializeField] private float spawnTime;
+    private List<GameObject> poolArrows;
     WaitForSecondsRealtime wait;
 
     void Start()
     {
+        poolArrows = new List<GameObject>();
+
         wait = new WaitForSecondsRealtime(spawnTime); //Store WaitForSeconds to avoid generating garbage
         StartCoroutine(Spawn());
     }
@@ -21,8 +24,23 @@ public class ArrowShooter : MonoBehaviour
         while (true)
         {
             yield return wait;
-            GameObject go = Instantiate(pfbArrow, transform.position, transform.rotation); //Spawn object in the right direction
-            go.GetComponent<Arrow>().Direction = transform.forward; //Set shot direction (same as the arrow shooter)
+            GameObject go = GenerateArrow();
+            go.GetComponent<Arrow>().Throw(transform.forward); //Set shot direction and shoot (same as the arrow shooter)
         }
+    }
+
+    private GameObject GenerateArrow()
+    {
+        for(int i = 0; i < poolArrows.Count; i++)
+        {
+            if (!poolArrows[i].activeSelf){
+                poolArrows[i].SetActive(true);
+                poolArrows[i].transform.position = transform.position; //Volver a la posición del lan
+                poolArrows[i].transform.rotation = transform.rotation;
+                return poolArrows[i];
+            }
+        }
+        poolArrows.Add(Instantiate(pfbArrow, transform.position, transform.rotation));
+        return poolArrows[poolArrows.Count - 1];
     }
 }
